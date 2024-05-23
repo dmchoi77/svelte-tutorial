af8ed5674f2794bd975838f283f736fa7091954e
최동민 <dm.choi@logiall.com>
2024-05-23 22:29:24 +0900

    refact: Slider 컴포넌트 분리, leftIndex, rightIndex를 Reactive Statement로 변경

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Slider from "./components/Slider.svelte";
+
   let product = {
     id: "svelte-book",
     name: "Svete Guide",
@@ -9,24 +11,6 @@
       "https://github.com/developer-book/svelte/raw/main/static/svelte-book-3.png",
     ],
   };
-
-  let sliderCenterIndex = 0;
-  let sliderLeftIndex = product.images.length - 1;
-  let sliderRightIndex = 1;
-
-  function sliderMoveLeft() {
-    const length = product.images.length;
-    sliderCenterIndex = (sliderCenterIndex - 1 + length) % length;
-    sliderLeftIndex = (sliderCenterIndex - 1 + length) % length;
-    sliderRightIndex = (sliderCenterIndex + 1) % length;
-  }
-
-  function sliderMoveRight() {
-    const length = product.images.length;
-    sliderCenterIndex = (sliderCenterIndex + 1) % length;
-    sliderLeftIndex = (sliderCenterIndex - 1 + length) % length;
-    sliderRightIndex = (sliderCenterIndex + 1) % length;
-  }
 
   let relatedProducts = [
     {
@@ -106,40 +90,6 @@
   .image-container img {
     width: 100%;
   }
-
-  .slider {
-    position: relative;
-    width: 80%;
-    margin: 0 10%;
-  }
-
-  .slider-item {
-    width: 100%;
-  }
-
-  .slider-item.left {
-    position: absolute;
-    top: 0;
-    right: 100%;
-  }
-
-  .slider-item.right {
-    position: absolute;
-    top: 0;
-    left: 100%;
-  }
-
-  .slider-left-button {
-    position: absolute;
-    top: 50%;
-    right: 100%;
-  }
-
-  .slider-right-button {
-    position: absolute;
-    top: 50%;
-    left: 100%;
-  }
 </style>
 
 <header class="header">
@@ -155,28 +105,7 @@
 <article class="product">
   <div class="product-main">
     <div class="image-container">
-      <div class="slider">
-        <img
-          src="{product.images[sliderLeftIndex]}"
-          alt="sliderImage(left)"
-          class="slider-item left"
-        />
-        <img
-          src="{product.images[sliderCenterIndex]}"
-          alt="sliderImage"
-          class="slider-item"
-        />
-        <img
-          src="{product.images[sliderRightIndex]}"
-          alt="sliderImage(right)"
-          class="slider-item right"
-        />
-        <button class="slider-left-button" on:click="{sliderMoveLeft}">-</button
-        >
-        <button class="slider-right-button" on:click="{sliderMoveRight}"
-          >+</button
-        >
-      </div>
+      <Slider images="{product.images}" />
     </div>
     <div>
       <h2>{product.name}</h2>
---
+++ src/components/Slider.svelte
@@ -0,0 +1,68 @@
+<script>
+  export let images;
+
+  let centerIndex = 0;
+  // $: 의존하고 있는 상태의 변화에 따라 값 변경 의미
+  $: leftIndex = (centerIndex - 1 + images.length) % images.length;
+  $: rightIndex = (centerIndex + 1) % images.length;
+
+  function moveLeft() {
+    centerIndex = (centerIndex - 1 + images.length) % length;
+  }
+
+  function moveRight() {
+    centerIndex = (centerIndex + 1) % images.length;
+  }
+</script>
+
+<style>
+  .slider {
+    position: relative;
+    width: 80%;
+    margin: 0 10%;
+  }
+
+  .slider-item {
+    width: 100%;
+  }
+
+  .slider-item.left {
+    position: absolute;
+    top: 0;
+    right: 100%;
+  }
+
+  .slider-item.right {
+    position: absolute;
+    top: 0;
+    left: 100%;
+  }
+
+  .slider-left-button {
+    position: absolute;
+    top: 50%;
+    right: 100%;
+  }
+
+  .slider-right-button {
+    position: absolute;
+    top: 50%;
+    left: 100%;
+  }
+</style>
+
+<div class="slider">
+  <img
+    src="{images[leftIndex]}"
+    alt="sliderImage(left)"
+    class="slider-item left"
+  />
+  <img src="{images[centerIndex]}" alt="sliderImage" class="slider-item" />
+  <img
+    src="{images[rightIndex]}"
+    alt="sliderImage(right)"
+    class="slider-item right"
+  />
+  <button class="slider-left-button" on:click="{moveLeft}">-</button>
+  <button class="slider-right-button" on:click="{moveRight}">+</button>
+</div>
